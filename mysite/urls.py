b00bc544029ad8ee2827155837c512b4691dd6fe
kadinw <130396759+kadinw@users.noreply.github.com>
2024-06-11 17:21:55 -0700

    urls2

--- mysite/urls.py
+++ mysite/urls.py
@@ -25,7 +25,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path("blogging/", include('blogging.urls')),
+    path("/", include('blogging.urls')),
     path('polling/', include('polling.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name='login'),
     path('logout/', LogoutView.as_view(next_page='/'), name='logout'),    
